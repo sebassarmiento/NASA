6b5ba05507005ba050764758f2ff6fa7a56dec47
Sebastian Sarmiento <sebi_sarmiento@hotmail.com>
2018-08-28 14:10:09 -0400

    Images-display

--- src/components/earth.js
+++ src/components/earth.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import CircularProgress from '@material-ui/core/CircularProgress';
+import Grid from '@material-ui/core/Grid';
+
 
 
 
@@ -7,8 +9,12 @@ const API_UR = 'JsG393zPVDFLqXc7ZbnKHH6DYwpCi0OjFlWBsi81'
 
 const API_URL = 'https://images-api.nasa.gov/search?q=apollo'
 
+const styles = {
+  mainGrid: { justifyContent: 'center', textAlign: 'center', alignItems: 'center', alignContent: 'center' }
+}
+
 class App extends Component {
-  constructor(){
+  constructor() {
     super()
     this.state = {}
 
@@ -20,17 +26,17 @@ class App extends Component {
     this.getData = this.getData.bind(this)
   }
 
-  componentDidMount(){
+  componentDidMount() {
     this.getData(API_URL)
   }
 
-  getData(url){
+  getData(url) {
     fetch(url).then(d => d.json()).then(resp => this.setState({
       data: resp
     }))
   }
 
-  searchQuery(value){
+  searchQuery(value) {
     let newData = this.url + value
     this.getData(newData)
   }
@@ -41,22 +47,38 @@ class App extends Component {
 
     return (
       <div className="App">
-      <h1>EARTH PHOTOS</h1>
+        <h1>EARTH PHOTOS</h1>
 
-      <div style={{margin: 20, padding: 20 }} >
-      <input type="text" placeholder="Search photos..." style={{fontSize: 20}} ref={this.searchInput} />
-      <button onClick={() => this.searchQuery(searchNode.value) } style={{fontSize: 20}} >Search</button>
-      </div>
+        <div style={{ margin: 20, padding: 20 }} >
+          <input type="text" placeholder="Search photos..." style={{ fontSize: 20 }} ref={this.searchInput} />
+          <button onClick={() => this.searchQuery(searchNode.value)} style={{ fontSize: 20 }} >Search</button>
+        </div>
 
-      {
-        this.state.data ? console.log(this.state.data) : <CircularProgress />
-      }
+        {
+          this.state.data ? console.log(this.state.data) : <CircularProgress />
+        }
 
-      {
-        this.state.data ? this.state.data.collection.items.map(item => {
-          return ( <div key={Math.random()} ><img src={item.links ? item.links[0].href : null } height={100} alt={item.data[0].description } /></div> )
-        }) : <CircularProgress />
-      }
+        <Grid container style={styles.mainGrid} >
+
+            {
+              this.state.data ? this.state.data.collection.items.map(item => {
+                return (
+
+                  item.links ? 
+                  <Grid item key={Math.random()} style={{margin: 'auto'}} >
+                    
+                      <img src={item.links[0].href} alt={item.data[0].description} style={{ maxHeight: 200}} />
+                    
+                  </Grid>
+
+                  : null
+
+                  
+                )
+              }) : <CircularProgress />
+            }
+
+            </Grid>
 
       </div>
     );
